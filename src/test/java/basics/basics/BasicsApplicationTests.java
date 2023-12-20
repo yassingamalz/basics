c1c78d21461e8cc69292ae0cb6a636d68f63f3bb
yassingamalz <yassen.gamal.yg@gmail.com>
2023-12-20 12:15:22 +0100

    initial commit

---
+++ src/test/java/basics/basics/BasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package basics.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
